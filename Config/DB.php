c116ab7eeace40ee471dea2851ab64aab8307a5a
Foad Ahmed <158590457+anonymousje@users.noreply.github.com>
2025-08-19 20:22:41 +0500

    Update DB.php

--- Config/DB.php
+++ Config/DB.php
@@ -1,14 +1,21 @@
 <?php
-
+ 
 namespace App\Config;
-
+ 
 use PDO;
 use PDOException;
-
+ 
 class DB {
     public static function getConnection(): PDO {
+        $host = 'dev-usmank-mysql-db.capu4w8cgzz7.us-east-1.rds.amazonaws.com';       
+        $port = '3306';
+        $dbname = 'clockwise';
+        $username = 'clockwiseuser';
+        $password = 'clockwise123';
+ 
         try {
-            $pdo = new PDO('mysql:host=localhost;dbname=clockwise', 'admin_u_rw', 'Loc@lhost');
+            $dsn = "mysql:host={$host};port={$port};dbname={$dbname};charset=utf8mb4";
+            $pdo = new PDO($dsn, $username, $password);
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $pdo;
         } catch (PDOException $e) {
